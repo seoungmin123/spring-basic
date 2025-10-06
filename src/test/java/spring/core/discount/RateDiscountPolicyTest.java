e4c6a470918f2967a951a7a525daae0967093f02
iseungmin <sm1111176@gmail.com>
2025-10-06 18:48:57 +0900

    feat : 쿠폰 정책 추가 (순수자바)

---
+++ src/test/java/spring/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,44 @@
+package spring.core.discount;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import spring.core.member.Grade;
+import spring.core.member.Member;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP일때 정률 할인 ")
+    void vip_할인성공(){
+        //given
+        Long memberId = 1L;
+        Member memberA = new Member(memberId, "memberA", Grade.VIP);
+
+        //when
+        int discountPrice = discountPolicy.discount(memberA, 10000);
+
+        //then
+        assertThat(discountPrice).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("BASIC일때 할인 안됨")
+    void basic_할인안됨(){
+        //given
+        Long memberId = 2L;
+        Member memberB = new Member(memberId, "memberB", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(memberB, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+
+}
